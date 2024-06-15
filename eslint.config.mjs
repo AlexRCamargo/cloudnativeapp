8b8db13cff4787ded624830c337147bff95555d9
AlexRCamargo <alexrcamargoti@gmail.com>
2024-06-15 14:58:37 -0300

    Atualizando projeto raiz

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,18 @@ import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
+  "eslint", "js.configs.recommended",
+  "plugin:react/recommended",
+
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   ...fixupConfigRules(pluginReactConfig),
+
+  "rules", {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"]
+  
+  }
 ];
