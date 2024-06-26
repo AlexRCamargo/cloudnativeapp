86b4a51a9abccdfeedd948a551bb72a3891abbe1
AlexRCamargo <alexrcamargoti@gmail.com>
2024-06-26 20:31:54 -0300

    Aplicação Frontend

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cloud Native App</title>
+        <meta name="description" content="Cloud Native App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome to Cloud Native App!</h1>
+    </div>
+    );
+}
